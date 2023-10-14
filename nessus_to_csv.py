ecc7c96c5a508d6280980f237b0d6e135edd4325
Sander van Hulst <sandervanhulst@MacBook-Air-van-Sander.local>
2023-10-14 13:51:34 +0200

    Rewritten, added CLI input option for single input file, added processing for port and CVE list

--- nessus_to_csv.py
+++ nessus_to_csv.py
@@ -1,48 +1,49 @@
-"""This script converts all Nessus files in a direcotry 
-into one CSV file."""
 #!/usr/bin/env python3
-
-# pylint: disable=I0011
-# pylint: disable=C0103
 
 import xml.etree.ElementTree as etree
 import csv
 import os
+import argparse
 
+def parse_arguments():
+	parser = argparse.ArgumentParser(description="Converts Nessus files into a CSV file.")
+	group = parser.add_mutually_exclusive_group(required=True)
+	group.add_argument("-i", "--input_dir", type=str, help="Directory containing the Nessus files")
+	group.add_argument("-f", "--input_file", type=str, help="Path to a single Nessus file")
+	parser.add_argument("-o", "--output_file", type=str, required=True, help="The path to the output CSV file")
+	parser.add_argument("-p", "--product", type=str, required=True, help="Product name to include in the CSV")
+	return parser.parse_args()
+	
+def process_nessus_file(file_path, csvwriter, product_name):
+    tree = etree.parse(file_path)
+    root = tree.getroot()
 
-ip = []
-vuln = []
-severity = []
+    for reportHost in root.iter('ReportHost'):
+        ip = reportHost.get('name')
 
-start = 1
+        for reportItem in reportHost.iter('ReportItem'):
+            vuln = reportItem.get('pluginName')
+            severity = reportItem.get('severity')
+            port = reportItem.get('port')  
+            # Extracting CVE information
+            cves = reportItem.findall('cve')
+            cve_list = ', '.join(cve.text for cve in cves) if cves else 'N/A'
+            data = [product_name, ip, port, vuln, severity, cve_list]  # Including product name in the data row
+            csvwriter.writerow(data)
 
+def main(input_dir, input_file, output_file, product_name):
+    with open(output_file, 'w', newline='') as nessus_file:
+        csvwriter = csv.writer(nessus_file)
+        csvwriter.writerow(['Product', 'IP Address', 'Port', 'Vulnerability', 'Severity', 'CVEs'])
 
-nessus_file = open('/tmp/nessus_file.csv', 'w')
-csvwriter = csv.writer(nessus_file)
-csvwriter.writerow(['ip Address', 'vulnerability', 'severity'])
+        if input_dir:
+            for fileName in os.listdir(input_dir):
+                if fileName.endswith(".nessus"):
+                    fullPath = os.path.join(input_dir, fileName)
+                    process_nessus_file(fullPath, csvwriter, product_name)
+        elif input_file:
+            process_nessus_file(input_file, csvwriter, product_name)
 
-count = 0
-i = 0
-
-data = []
-
-for fileName in os.listdir("."):
-    if ".nessus" in fileName:
-        if start:
-            mainTree = etree.parse(fileName)
-            root = mainTree.getroot()
-            for reportHost in root.iter('ReportHost'):
-                ip.append(reportHost.get('name'))
-            for reportItem in root.iter('ReportItem'):
-                vuln.append(reportItem.get('pluginName'))
-                severity.append(reportItem.get('severity'))
-            # Only go through the while loop while there are still boxes (with IPs) to process
-            while i < len(ip):
-                data.append(ip[count])
-                data.append(vuln[count])
-                data.append(severity[count])
-                csvwriter.writerow(data)
-                data = []
-                i += 1
-                count += 1
-nessus_file.close()
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args.input_dir, args.input_file, args.output_file, args.product)
